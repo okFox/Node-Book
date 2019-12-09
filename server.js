c59d559ac0de822007d913da61c829761b72d371
okFox <smayfield@gmail.com>
2019-12-08 21:02:08 -0800

    router js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const url = require('url');
 
-const start = () => {
+const start = (route, handle) => {
     function onRequest(req, res) {
-        console.log('request received...');
+        const pathname = url.parse(req.url).pathname;
+        console.log(`Request for ${pathname} received.`);
+        route(handle, pathname);
+
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('Hello Node!');
         res.end();
