513437c0245666ce2634e75e6f4eb6acea683143
okFox <smayfield@gmail.com>
2019-12-08 19:53:33 -0800

    index js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello Node!!');
-    res.end();
-}).listen(8888);
+const start = () => {
+    function onRequest(req, res) {
+        console.log('request received...');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('Hello Node!');
+        res.end();
+    }
+    
+    http.createServer(onRequest).listen(8888);
+    console.log('listening on port 8888');
+};
+
+exports.start = start;
